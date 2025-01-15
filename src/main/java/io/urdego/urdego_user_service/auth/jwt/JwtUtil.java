c879daf3d3158b5fc3b602a5db10cd248ac77f87
Ban-gilhyeon <bbgiloo98@gmail.com>
2025-01-15 16:25:07 +0900

    Refactor(#2) : CI/CD build Fail 수정

--- src/main/java/io/urdego/urdego_user_service/auth/jwt/JwtUtil.java
+++ src/main/java/io/urdego/urdego_user_service/auth/jwt/JwtUtil.java
@@ -10,7 +10,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SecurityException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
