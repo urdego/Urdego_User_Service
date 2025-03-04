04894e767fd995a6732fef6f9e5342ecfab0096f
JoungWoo Lee <j_ra1n@naver.com>
2025-03-04 19:05:55 +0900

    Feature(#34): 모니터링 의존성 추가

--- src/main/java/io/urdego/urdego_user_service/UrdegoUserServiceApplication.java
+++ src/main/java/io/urdego/urdego_user_service/UrdegoUserServiceApplication.java
@@ -12,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableConfigurationProperties
 public class UrdegoUserServiceApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UrdegoUserServiceApplication.class, args);
 	}
